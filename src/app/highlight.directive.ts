5ce313626944de9dd34d9c39022abb377f098dcf
lan <lupamo93@gmail.com>
2018-05-27 23:50:43 +0300

    pipe data + pipe transform,

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,13 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem: ElementRef) {
+    console.log(elem)
+    this.elem.nativeElement.style.boxShadow='1px 80px 10px 20px rgba(216, 48, 48, 0.596)';
+  }
 
 }
