540ddf6e1bfd8b41f55970f7c077ff333116c6b8
lan <lupamo93@gmail.com>
2018-05-27 23:19:06 +0300

    quotes.ts|| constructor function

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,3 +1,12 @@
 export class Quotes {
-  constructor(public id:number; public name:string, public date:Date; public showDetails: boolean )
+    public id:number; public author:string; public txtQuote:string; public showDetails: boolean; public date:Date; public vote:number;
+    public maxCount:number=0;
+    constructor(id,author,txtQuote, date){
+        this.id = id;
+        this.author = author;
+        this.txtQuote = txtQuote;
+        this.date = date;
+        this.vote = 0;
+        this.showDetails = false;
+    }
 }
