dbdf598dbd2ba391aa671e6ee59672289f1245eb
lan <lupamo93@gmail.com>
2018-05-27 23:21:36 +0300

    addition of custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotesDetailsComponent
+    QuotesDetailsComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
