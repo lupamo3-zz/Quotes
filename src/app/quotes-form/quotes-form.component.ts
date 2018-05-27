b86abb07a216a854902fc59af30ef6ddd0c9d176
lan <lupamo93@gmail.com>
2018-05-27 23:53:10 +0300

    quotes form component,

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+
+  newQuoteForm = new Quotes(0,"","",new Date());
+  // newQuoteForm = [new Quotes(0,"","")];
+  @Output() addNewQuote = new EventEmitter<Quotes>();
+
+  submitQuote(newOne){
+    // this.newQuoteForm.push(new Quotes(newOne.value.id++,newOne.value.author,newOne.value.author));
+    // console.log(newOne.value.date);
+    // this.newQuoteForm.push(newOne);
+    this.addNewQuote.emit(new Quotes(newOne.value.id++,newOne.value.author,newOne.value.txtQuote,newOne.value.date));
+    // console.log(newOne.value.author);
+    newOne.reset();
+  }
 
   constructor() { }
 
